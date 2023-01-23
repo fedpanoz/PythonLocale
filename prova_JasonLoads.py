ad0ec95d4e6d486e51dbac6adb70b2862df14783
fedpanoz <federicopanozzo@gmail.com>
2023-01-23 15:44:52 +0100

    22.01

--- prova_JasonLoads.py
+++ prova_JasonLoads.py
@@ -2,8 +2,8 @@ from pathlib import Path
 import json
 oggetto = Path('listanumeri.json')
 da_leggere_noConversion = oggetto.read_text()
-print(da_leggere_noConversion)
-print(type(da_leggere_noConversion))
+#print(da_leggere_noConversion)
+#print(type(da_leggere_noConversion))
 da_leggere_converted = json.loads(da_leggere_noConversion)
 print(da_leggere_converted)
 print(type(da_leggere_converted))
